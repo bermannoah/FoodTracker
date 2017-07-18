530b0604d9dbd5e1b643e1320fccbf355281b6c0
Noah Berman <noah@noahberman.org>
2017-07-18 22:00:33 +0200

    Add unit tests

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,38 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Noah Berman on 7/18/17.
+//  Copyright © 2017 Faceless Company. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The name ust be between 0 and 5 invlusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
