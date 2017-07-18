a08d4dcb05bdd6972cef41d7a905d6e2f9d43e6b
Noah Berman <noah@noahberman.org>
2017-07-18 20:27:39 +0200

    Add basic delegate logic

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-    
     @IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +27,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
     // MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
